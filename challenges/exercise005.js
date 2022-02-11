011d6911501aa5388f89a01b356dae0a2c4e8510
Rachel Miles <rachmiles@gmail.com>
2022-02-11 13:49:41 +0000

    Replace var by const / let in exercise005

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -1,33 +1,37 @@
 const findNextNumber = (nums, n) => {
   if (nums === undefined) throw new Error("nums is required");
   if (n === undefined) throw new Error("n is required");
-  if (nums.indexOf(n) === -1 || nums.indexOf(n) === nums.length - 1) return null;
+  if (nums.indexOf(n) === -1 || nums.indexOf(n) === nums.length - 1)
+    return null;
   return nums[nums.indexOf(n) + 1];
 };
 
-const count1sand0s = str => {
+const count1sand0s = (str) => {
   if (str === undefined) throw new Error("str is required");
-  return { 0: (str.match(/0/g) || []).length, 1: (str.match(/1/g) || []).length};
+  return {
+    0: (str.match(/0/g) || []).length,
+    1: (str.match(/1/g) || []).length,
+  };
 };
 
-const reverseNumber = n => {
+const reverseNumber = (n) => {
   if (n === undefined) throw new Error("n is required");
-  return parseFloat(n.toString().split('').reverse().join(''));
+  return parseFloat(n.toString().split("").reverse().join(""));
 };
 
-const sumArrays = arrs => {
+const sumArrays = (arrs) => {
   if (arrs === undefined) throw new Error("arrs is required");
-  var result = 0;
-  arrs.forEach(function(item) {
+  let result = 0;
+  arrs.forEach(function (item) {
     result += item.reduce((a, b) => a + b, 0);
   });
   return result;
 };
 
-const arrShift = arr => {
+const arrShift = (arr) => {
   if (arr === undefined) throw new Error("arr is required");
   if (arr.length < 2) return arr;
-  var result = arr.slice();
+  const result = arr.slice();
   result[0] = arr[arr.length - 1];
   result[arr.length - 1] = arr[0];
   return result;
@@ -37,20 +41,27 @@ const findNeedle = (haystack, searchTerm) => {
   if (haystack === undefined) throw new Error("haystack is required");
   if (searchTerm === undefined) throw new Error("searchTerm is required");
   for (let key in haystack) {
-    if(typeof haystack[key] === "string" && haystack[key].toLowerCase().includes(searchTerm.toLowerCase())) return true;
+    if (
+      typeof haystack[key] === "string" &&
+      haystack[key].toLowerCase().includes(searchTerm.toLowerCase())
+    )
+      return true;
   }
   return false;
 };
 
-const getWordFrequencies = str => {
+const getWordFrequencies = (str) => {
   if (str === undefined) throw new Error("str is required");
   // split the string into an array of individual words and remove the punctuation
-  var words = [];
-  str.toLowerCase().split(" ").forEach(word => words.push(word.replace(/\W|_/g, "")));
+  const words = [];
+  str
+    .toLowerCase()
+    .split(" ")
+    .forEach((word) => words.push(word.replace(/\W|_/g, "")));
 
-  // populate frequencies 
-  var frequencies = {};
-  words.forEach(word => {
+  // populate frequencies
+  const frequencies = {};
+  words.forEach((word) => {
     if (frequencies[word] === undefined) {
       frequencies[word] = 1;
     } else {
@@ -67,5 +78,5 @@ module.exports = {
   sumArrays,
   arrShift,
   findNeedle,
-  getWordFrequencies
+  getWordFrequencies,
 };

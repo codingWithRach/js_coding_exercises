a9019d9f0c8b05eda0b5f44e9ac31c2a6c5f327b
Rachel Miles <rachmiles@gmail.com>
2022-02-14 12:07:08 +0000

    filter and map changed to use arrow notation in exercise004

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,75 +1,53 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  // using filter
-  const result = nums.filter(function (item) {
-    return item < 1;
-  });
-  return result;
+  // using filter with arrow notation
+  return nums.filter((item) => item < 1);
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  // using filter
-  const result = names.filter(function (item) {
-    return item.substring(0, 1) === char;
-  });
-  return result;
+  // using filter with arrow notation
+  return names.filter((item) => item.substring(0, 1) === char);
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  // using filter
-  const result = words.filter(function (item) {
-    return item.substring(0, 3) === "to ";
-  });
-  return result;
+  // using filter with arrow notation
+  return words.filter((item) => item.substring(0, 3) === "to ");
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  // using filter
-  const result = nums.filter(function (item) {
-    return Number.isInteger(item);
-  });
-  return result;
+  // using filter with arrow notation
+  return nums.filter((item) => Number.isInteger(item));
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
-  // using map
-  const result = users.map(function (item) {
-    return item.data.city.displayName;
-  });
-  return result;
+  // using map with arrow notation
+  return users.map((item) => item.data.city.displayName);
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-  // using map
-  const result = nums.map(function (item) {
-    return +Math.sqrt(item).toFixed(2);
-  });
-  return result;
+  // using map with arrow notation
+  return nums.map((item) => +Math.sqrt(item).toFixed(2));
 }
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  // using filter
-  const result = sentences.filter(function (item) {
-    return item.toLowerCase().includes(str.toLowerCase());
-  });
-  return result;
+  // using filter with arrow notation
+  return sentences.filter((item) =>
+    item.toLowerCase().includes(str.toLowerCase())
+  );
 }
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-  // using map
-  const result = triangles.map(function (item) {
-    return Math.max(...item);
-  });
-  return result;
+  // using map with arrow notation
+  return triangles.map((item) => Math.max(...item));
 }
 
 module.exports = {
@@ -80,5 +58,5 @@ module.exports = {
   getCities,
   getSquareRoots,
   findSentencesContaining,
-  getLongestSides
+  getLongestSides,
 };

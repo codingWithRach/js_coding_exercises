155fda3ca79ad149407eef0295f3b7361909f048
Rachel Miles <rachmiles@gmail.com>
2022-02-17 08:53:08 +0000

    Replace for loops in exercise003

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,52 +1,40 @@
 function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
-  for (let index = 0; index < nums.length; ++index) {
-    nums[index] = Math.pow(nums[index], 2);
-  }
-  return nums;
+  return nums.map((num) => num * num);
 }
 
 function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
-  let camel = "";
-  for (let index = 0; index < words.length; ++index) {
-    // replace if / else with ternary operator
-    index === 0
-      ? (camel += words[index])
-      : (camel +=
-          words[index].substring(0, 1).toUpperCase() +
-          words[index].substring(1));
-  }
-  return camel;
+  return words
+    .map((word) => {
+      return word === words[0]
+        ? word.toLowerCase()
+        : word.charAt(0).toUpperCase() + word.substring(1);
+    })
+    .join("");
 }
 
 function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
   let numSubjects = 0;
-  for (let index = 0; index < people.length; ++index) {
-    numSubjects += people[index].subjects.length;
-  }
+  people.forEach((person) => (numSubjects += person.subjects.length));
   return numSubjects;
 }
 
 function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  for (let index = 0; index < menu.length; ++index) {
-    if (menu[index].ingredients.includes(ingredient)) return true;
-  }
-  return false;
+  return (
+    menu.filter((menuItem) => menuItem.ingredients.includes(ingredient))
+      .length > 0
+  );
 }
 
 function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
-  const result = [];
-  for (let index = 0; index < arr1.length; ++index) {
-    if (arr2.includes(arr1[index]) && !result.includes(arr1[index]))
-      result.push(arr1[index]);
-  }
-  return result.sort();
+  // the Set operator removes the duplicates
+  return [...new Set(arr1.filter((num) => arr2.includes(num)))].sort();
 }
 
 module.exports = {

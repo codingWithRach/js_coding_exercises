a1a838a1e9ffaebc64bf2e537ab86c87101a51f4
Rachel Miles <rachmiles@gmail.com>
2022-02-28 14:23:46 +0000

    Remove unnecessary comments

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -7,38 +7,32 @@ function findSmallNums(nums) {
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  // using filter with arrow notation, and replace substring by startsWith
   return names.filter((item) => item.startsWith(char));
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  // using filter with arrow notation
   return words.filter((item) => item.substring(0, 3) === "to ");
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  // using filter with arrow notation
   return nums.filter((item) => Number.isInteger(item));
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
-  // using map with arrow notation
   return users.map((item) => item.data.city.displayName);
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-  // using map with arrow notation
   return nums.map((item) => +Math.sqrt(item).toFixed(2));
 }
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  // using filter with arrow notation
   return sentences.filter((item) =>
     item.toLowerCase().includes(str.toLowerCase())
   );
@@ -46,7 +40,6 @@ function findSentencesContaining(sentences, str) {
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-  // using map with arrow notation
   return triangles.map((item) => Math.max(...item));
 }
 
